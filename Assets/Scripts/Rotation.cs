082753b02188898a56feba8e9301c6549271623f
JMShah108 <janiya.shah2028@gmail.com>
2023-09-24 19:15:42 -0400

    A colorful 2D obstacle game built in Unity. Dodge the obstacles to get to the portal. Good luck!

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+   public float rotateSpeed;
+
+   void Start() {
+
+   }
+
+   void Update() {
+    transform.Rotate(0,0, rotateSpeed * Time.deltaTime);
+   }
+}
